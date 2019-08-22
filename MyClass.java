76459c919941ad257fec2882a20875b0ab223ff5
oz kilim <ozkilim@hotmail.co.uk>
2019-08-22 16:23:41 +0200

    stars

--- MyClass.java
+++ MyClass.java
@@ -1,11 +1,23 @@
+// printing stars
+
+
 public class MyClass {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  
-  for (int i = 0; i < 60; i++) {
-  	System.out.println(i);
-		}
-	}
+  public static void main(String[] args) 
+{
+int i,j,k;
+for(i=1; i<=5; i++)
+{
+for(j=4; j>=i; j--)
+{
+System.out.print(" ");
+}
+for(k=1; k<=(2*i-1); k++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+}
 }
 
 
