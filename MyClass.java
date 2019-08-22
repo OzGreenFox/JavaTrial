801ef1188d9d0bae557b5d0c492eb98b2ca0e60a
oz kilim <ozkilim@hotmail.co.uk>
2019-08-22 13:13:24 +0200

    longer loop

--- MyClass.java
+++ MyClass.java
@@ -2,7 +2,7 @@ public class MyClass {
   public static void main(String[] args) {
     System.out.println("Hello World");
   
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 50; i++) {
   	System.out.println(i);
 		}
 	}
