69787bc77c60c512626bdc4f1e8abea9c5827be0
oz kilim <ozkilim@hotmail.co.uk>
2019-08-23 12:32:10 +0200

    stars printing

--- MyClass.java
+++ MyClass.java
@@ -1,17 +1,24 @@
 // printing stars
 
 
+   //     	 *
+   //      *  *
+   //     *  *  *
+   //   *  *  *  *
+   //  *  *  *  *  *
+
+
 public class MyClass {
   public static void main(String[] args) 
 {
 int i,j,k;
-for(i=1; i<=5; i++)
+for(i=1; i<=10; i++) //defines rows
 {
-for(j=4; j>=i; j--)
+for(j=10; j>=i; j--) // j is defined as a shrinking number
 {
 System.out.print(" ");
 }
-for(k=1; k<=(2*i-1); k++)
+for(k=1; k<=(2*i-1); k++) // k prints the stars the number grows ultill...
 {
 System.out.print("*");
 }
