dd5e9e476f0ccf5ebd88521b26d6b69130bac76c
oz kilim <ozkilim@hotmail.co.uk>
2019-08-23 12:43:49 +0200

    first list commit

--- StreamExcersises.java
+++ StreamExcersises.java
@@ -1,3 +1,11 @@
-public class MyClass {
-  public static void main(String[] args) 
+// Ex1
+// Write a Stream Expression to get the even numbers from the following list:
 
+
+
+
+ public class StreamExcersises {
+  public static void main(String[] args) { 
+  List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+ }
+}
